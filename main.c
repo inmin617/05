dab0f3153982ad371308ff082632679de331737c
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-10-05 16:35:14 +0900

    실습6

--- main.c
+++ main.c
@@ -6,21 +6,27 @@
 int main(int argc, char *argv[]) 
 {
 	
-	int sum=0;
+	int answer=59;
 	int num;
-	int i;
+	int trial=0;
 	
-	printf("input a number:");
-	scanf("%d", &num);
-	
-	for (i=0; i<=num; i++)
-	
+	do
 	{
-		sum=sum+i;
+		printf("Guess a number:");
+		scanf("%d", &num);
 		
-		}	
+		if (num>answer)
+			printf("high!\n");
+		else if (num<answer)
+			printf("low!\n");
+			
+		trial=trial+1;
 		
-	printf("result is %i\n", sum);
+	}
+	
+	while(num != answer);
+	
+	printf("Congratulation! trials: %d\n", trial);
 	
 	system("PAUSE");
 	return 0;
